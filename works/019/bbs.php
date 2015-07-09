d1600465e47b170c1b4463cf10b67499777e51bd
Seiya Tanaka <seintoseiya2@gmail.com>
2015-07-09 17:37:07 +0200

    The first commit for 019. chat（前回のcommitはミス。今回が正規版。）

--- works/019/bbs.php
+++ works/019/bbs.php
@@ -1,3 +1,43 @@
 <?php
+$testFile = "test.log";
+if($_GET['mode'] == "0"){
+$user = htmlspecialchars($_GET['user'], ENT_QUOTES, "utf-8");
+$message = htmlspecialchars($_GET['message'], ENT_QUOTES, "utf-8");
+$inputValue = "<div class='user'>".$user."</div><div class='left_balloon'>".$message."</div>";
 
-echo htmlspecialchars($.GET['name'], ENT_QUOTES, "utf-8");
+
+
+// ファイルにデータを書き込み
+if($inputValue){
+	// ファイルをオープンできたか
+	if(!$fp = fopen($testFile, "a")){
+		echo "could not open";
+		exit;
+	}
+	// 書き込みできたか
+	if(fwrite($fp, $inputValue) === false) {
+		echo "could not write";
+		exit;
+	}
+	// 終了処理
+	fclose($fp);
+} else {
+	echo "not writable";
+	exit;
+}
+
+// ファイルからデータを読み込み
+// if(!$fp = fopen($testFile, "r")){
+// 	echo "could not open";
+// 	exit;
+// }
+// $outputValue = fread($fp, filesize($testFile));
+// echo $outputValue;
+// fclose($fp);
+$outputValue = file_get_contents($testFile);
+echo $outputValue;
+
+}else{
+	$outputValue = file_get_contents($testFile);
+	echo $outputValue;
+}
--- works/019/js/main.js
+++ works/019/js/main.js
@@ -1,9 +1,44 @@
 $(function(){
-    $('#button').click(function(){
+    $('#greet').click(function(){
+    	if(!$('#user').val() || !$('#message').val()) return;
         $.get('bbs.php', {
-        	name: $('#message').val()
+        	user: $('#user').val(),
+        	message: $('#message').val(),
+        	mode: "0" // 書き込み
         }, function(data){
         	$('#result').html(data);
+        	// scTarget();
         });
     });
+    loadLog();
+    logAll();
 });
+
+// ログをロードする
+function loadLog(){
+	$.get('bbs.php', {
+		mode: "1" // 読み込み
+    }, function(data){
+    	$('#result').html(data);
+    	// scTarget();
+    });
+}
+
+// 一定間隔でログをリロードする
+function logAll(){
+	setTimeout(function(){
+		loadLog();
+		logAll();
+	},5000); //リロード時間はここで調整
+}
+
+/*
+ * 画面を最下部へ移動させる
+ */
+// function scTarget(){
+// 	var pos = $("#end").offset().top; 
+// 	$("#talkField").animate({ 
+// 		scrollTop:pos
+// 	}, 0, "swing"); //swingで0が良さそう
+// 	return false;
+// }
