5d41137a6b613e7b2c4ba4abe9c4711dd1cb842f
Seiya Tanaka <seintoseiya2@gmail.com>
2015-07-27 15:48:05 +0200

    画像を全画面表示に変更

--- works/023/js/main.js
+++ works/023/js/main.js
@@ -9,66 +9,66 @@ $(function () {
 
         $('#bg1').css('background-position', '0 '+dy+'px');
 
-        if(dy > 480){
-            $('#bg2').css('background-position', '0 '+(dy-480)+'px');
+        if(dy > 800){
+            $('#bg2').css('background-position', '0 '+(dy-800)+'px');
         }else{
             $('#bg2').css('background-position', '0 0');
         }
-        if(dy > 960){
-            $('#bg3').css('background-position', '0 '+(dy-960)*2+'px');
+        if(dy > 1600){
+            $('#bg3').css('background-position', '0 '+(dy-1600)+'px');
         }else{
             $('#bg3').css('background-position', '0 0');
         }
         // パ
-        if(dy > 960){
-            $('#msg1').css('opacity', (dy-960)/480);
+        if(dy > 1600){
+            $('#msg1').css('opacity', (dy-1600)/800);
             $('#msg1').css('top', 100);
-            var dx = (dy-960) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-960);
+            var dx = (dy-1600) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1600);
             $('#msg1').css('left', dx);
         }else{
             $('#msg1').css('opacity', 0);
         }
         // ラ
-        if(dy > 1060){
-            $('#msg2').css('opacity', (dy-960)/480);
+        if(dy > 1700){
+            $('#msg2').css('opacity', (dy-1700)/800);
             $('#msg2').css('top', 160);
-            var dx = (dy-1060) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1060);
+            var dx = (dy-1700) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1700);
             $('#msg2').css('right', dx);
         }else{
             $('#msg2').css('opacity', 0);
         }
         // ラ
-        if(dy > 1160){
-            $('#msg3').css('opacity', (dy-960)/480);
+        if(dy > 1800){
+            $('#msg3').css('opacity', (dy-1800)/800);
             $('#msg3').css('top', 220);
-            var dx = (dy-1160) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1160);
+            var dx = (dy-1800) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1800);
             $('#msg3').css('left', dx);
         }else{
             $('#msg3').css('opacity', 0);
         }
         // ッ
-        if(dy > 1260){
-            $('#msg4').css('opacity', (dy-960)/480);
+        if(dy > 1900){
+            $('#msg4').css('opacity', (dy-1900)/800);
             $('#msg4').css('top', 280);
-            var dx = (dy-1260) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1260);
+            var dx = (dy-1900) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1900);
             $('#msg4').css('right', dx);
         }else{
             $('#msg4').css('opacity', 0);
         }
         // ク
-        if(dy > 1360){
-            $('#msg5').css('opacity', (dy-960)/480);
+        if(dy > 2000){
+            $('#msg5').css('opacity', (dy-2000)/800);
             $('#msg5').css('top', 340);
-            var dx = (dy-1360) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1360);
+            var dx = (dy-2000) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-2000);
             $('#msg5').css('left', dx);
         }else{
             $('#msg5').css('opacity', 0);
         }
         // ス
-        if(dy > 1460){
-            $('#msg6').css('opacity', (dy-960)/480);
+        if(dy > 2100){
+            $('#msg6').css('opacity', (dy-2100)/800);
             $('#msg6').css('top', 400);
-            var dx = (dy-1460) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-1460);
+            var dx = (dy-2100) > $(this).width()/2-24 ? $(this).width()/2-24 : (dy-2100);
             $('#msg6').css('right', dx);
         }else{
             $('#msg6').css('opacity', 0);
